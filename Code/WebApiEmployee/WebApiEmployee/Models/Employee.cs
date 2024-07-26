ee6dc99e14b9a91aec613d13f762b9401993fa5c
Thomas <calerovillalobos10@gmail.com>
2024-07-25 18:59:44 -0600

    Add employee model

---
+++ Code/WebApiEmployee/WebApiEmployee/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using System.Net;
+using System.Reflection;
+
+namespace WebApiEmployee.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public int FirstName { get; set; }   
+        public int LastName { get; set; }
+        public int DateOfBirth { get; set; }
+        public int Gender { get; set; }
+        public int MaritalStatus { get; set; }
+        public int Address { get; set; }
+        public int PhoneNumber { get; set; }
+        public int Email { get; set; }
+        public int HireDate { get; set; }
+        public int Salary { get; set; }
+        public int SupervisorID { get; set; }
+        public int EmploymentStatus { get; set; }
+        public int CreatedAt { get; set; }
+        public int UpdatedAt { get; set; }
+    }
+}
